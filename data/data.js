0c43cddf6eb21549489709973b9ef17e56416e81
Simply Done <olko4455@gmail.com>
2024-01-28 18:03:20 +0200

    first commit

---
+++ data/data.js
@@ -0,0 +1,113 @@
+export const ways = [
+  {
+    title: "Комплект",
+    description: "(бюстгальтер та трусики)",
+    characteristics: {
+      catag1:
+        "Виконаний з вишуканого мережива (вр-во Німеччина) та ніжного прозорого сітчастого полотна",
+      catag2:
+        "Стан посилено широкою еластичною стрічкою для надійної підтримки",
+      catag3: "Стрінги з регулюванням обсягу по лінії талії",
+    },
+    price: "$28,57",
+    contain: "60% поліамід, 35% поліестер, 5% еластан",
+    facture: "Ажурна",
+    image:
+      "./img/main/young-woman-transparent-lingerie-posing-against-studio-background-ratio-10x10-proportions-jpg-ratio-width-500-jpg.webp",
+  },
+  {
+    title: "Піжама",
+    description: "Очікується",
+    image:
+      "./img/main/medium-shot-smiley-woman-posing-studio-ratio-10x10-proportions-jpg-ratio-width-500-jpg.webp",
+    characteristics: {
+      catag1:
+        "Вишуканий комплект Hypnose з ніжного італійського атласу — топ + шортики.",
+      catag2: "Чудовий варіант для дому та для розкішного відпочинку.",
+      catag3:
+        "Топ прямого крою на тонких бретельках, що регулюються. Вставка мережива по лінії декольте додає особливого шарму та пікантності.",
+      catag4: "Короткі шортики на резинці, зі вставками мережива по краях.",
+      catag5: "Чудово сідають по фігурі та забезпечують повний комфорт.",
+    },
+  },
+  {
+    title: "Комплект",
+    description: "Топ та стягуюча спідниця",
+    price: "$42,86",
+    image:
+      "./img/main/smiley-senior-woman-wearing-shapewear-front-view-ratio-10x10-proportions-jpg-ratio-width-500-jpg.webp",
+    characteristics: {
+      catag1: "Мікрофіброві трксики з широким поясом, що стягує та бюстгалтер.",
+    },
+    contain: "90% нейлон, 10% спандекс",
+    facture: "Висока талія",
+  },
+  {
+    title: "Трусики",
+    price: "$10,71",
+    image:
+      "./img/main/quality-female-lingerie-still-life-ratio-10x10-proportions-jpg-ratio-width-500-jpg.webp",
+    characteristics: {
+      catag1:
+        "Бежеві стильні трусики - бразиліани із середньою посадкою, виконані з оригінального мережива (Німеччина).",
+      catag2:
+        "Ідеальна посадка та м'які ніжні еластичні стрічки забезпечують комфорт при повсякденному носінні.",
+      catag3: "Оздоблені стильними французькими бантиками позаду.",
+    },
+    contain: "Бразиліани",
+  },
+
+  {
+    title: "Весільний комплект",
+    description: "Бюстгальтер та трусики ",
+    price: "42,86",
+    image:
+      "./img/main/beautiful-long-legged-blonde-model-girl-with-perfect-sporty-body-with-stylish-wedding-hairstyle-fashionable-white-lace-lingerie-sits-bed-vintage-interior-ratio-10x10-proportions-jpg-ratio-width-500-jpg.webp",
+    characteristics: {
+      catag1:
+        "Забезпечує глибоку відкриту лінію декольте спереду та комфортне прилягання збоку",
+      catag2:
+        "Високі м’які трикутні чашечки з боковим рельєфом, посилені сітчастним полотном.",
+      catag3: "Збирає груди до центру.",
+      catag4: "Стильні трусики-стрінги з середньою посадкою.",
+      catag5:
+        "Виконані з розкішного італійського мережива, яке підкреслює кожен елемент оригінального малюнка.",
+    },
+  },
+  {
+    title: "Боді",
+    price: "$26,79",
+    image:
+      "./img/main/smiley-senior-woman-wearing-shapewear-front-view-ratio-10x10-proportions-jpg-ratio-width-500-jpg.webp",
+    characteristics: {
+      catag1: "Комфортне боді.",
+      catag2: "Виконано з 85% бавовни, 10% еластану та 5% поліаміду.",
+      catag3: "Підкреслює силует і дарує комфорт.",
+      catag4: "Ластовиця з 100% бавовняного полотна.",
+    },
+  },
+  {
+    title: "Комплект",
+    description: "Бюстгалтер та трусики",
+    price: "$46.43",
+    image:
+      "./img/main/bride-stands-with-her-back-white-silk-pajamas-ratio-10x10-proportions-jpg-ratio-width-500-jpg.webp",
+    characteristics: {
+      catag1: "Бюстгальтер з ексклюзивним італійським мереживом, до складу якого входить органічна бавовна.",
+      catag2: "Поєднує в собі стиль і натуральність - мереживо бездоганно виглядає і одночасно дуже ніжне до тіла.",
+      catag3: "Форма Helena: балконет на основі формованих чашок пуш-ап. Модель на стані та каркасах.",
+      catag4: "Оптимальний варіант одягу із глибоким вирізом."
+    }
+  },
+  {
+    title: "Бюстгалтер без кісточок",
+    price: "$23,21",
+    image: "./img/main/female-lingerie-arrangement-still-life-ratio-10x10-proportions-jpg-ratio-width-500-jpg.webp",
+    characteristics: {
+      catag1: "Бюстгальтер з ексклюзивним італійським мереживом, до складу якого входить органічна бавовна.",
+      catag2: "Оптимальний варіант для одягу з V-подібним вирізом.",
+      contain: "60% поліамід, 35% поліестер, 5% еластан",
+      facture: "Ажурна",
+    }
+  },
+];

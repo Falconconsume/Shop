efc73ac5fe7b2644ca66d9079572590ec12857c6
Yulia <yulia0903e@gmail.com>
2024-02-15 21:06:20 +0200

    add MainPage.jsx

--- src/AppWraper.jsx
+++ src/AppWraper.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import App from './App'
-import Slider from './components/Slider/Slider'
-import MiddlePart from './components/Middle-part/Middle-part'
 
 export default function AppWrapper() {
     return (
         <div>
             <App />
-            <Slider />
-            <MiddlePart />
         </div>
     )
 }
---
+++ src/MainPage.jsx
@@ -0,0 +1,11 @@
+import Slider from './components/Slider/Slider'
+import MiddlePart from './components/Middle-part/Middle-part'
+
+export default function MainPage() {
+    return (
+        <div>
+            <Slider />
+            <MiddlePart />
+        </div>
+    )
+}

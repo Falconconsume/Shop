6a76c5f35cd1ee3e9c50e8366576b1d7d185152d
Yulia <yulia0903e@gmail.com>
2024-02-18 21:05:05 +0200

    add Blog-part

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,11 +1,13 @@
 import Slider from './components/Slider/Slider'
 import MiddlePart from './components/Middle-part/Middle-part'
+import Blog from './components/Blog/Blog'
 
 export default function MainPage() {
     return (
         <div>
             <Slider />
             <MiddlePart />
+            <Blog />
         </div>
     )
 }
